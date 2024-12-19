c12ae9861935bbb01ed1ab6b132d3c44c8b462c1
AlexIJuravlev <cawasimus@gmail.com>
2024-12-19 17:27:03 +0300

    Recipe for dumplings

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,75 @@
+import { useState } from 'react';
 import styles from './app.module.css';
 import data from './data.json';
 
 export const App = () => {
 	// Можно задать 2 состояния — steps и activeIndex
+	const steps = data;
+	const [activeIndex, setActiveIndex] = useState(1);
+	const [dis, setDis] = useState(true);
 
 	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 
-	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	function clickUp() {
+		setActiveIndex(activeIndex + 1);
+		setDis(false);
+		if (activeIndex === steps.length) {
+			setActiveIndex(1);
+			setDis(true);
+		}
+	}
+
+	function clickDown() {
+		const prevIndex = activeIndex - 1;
+		setActiveIndex(prevIndex);
+		if (prevIndex === 1) {
+			setDis(true);
+		}
+	}
+
+	const itemPitch = steps.map((item, i) => {
+		const index = i + 1;
+		const activeStyle = activeIndex === index ? styles.active : '';
+		const doneStyle = activeIndex > index ? styles.done : '';
+		const fullStyle = `${styles['steps-item']} ${activeStyle} ${doneStyle}`;
+		const clickButton = () => {
+			setActiveIndex(index);
+		};
+
+		return (
+			<li onClick={clickButton} key={item.id} className={fullStyle}>
+				<button key={item.id} className={`${styles['steps-item-button']} `}>
+					{index}
+				</button>
+				Шаг {index}
+			</li>
+		);
+	});
+
+	const activePitch = steps.find((a, i) => i + 1 === activeIndex);
+
+	const title = <h1 key={activePitch.id}>{activePitch.title}</h1>;
+	const content = <div className={styles['steps-content']}>{activePitch.content}</div>;
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
-				<h1>Инструкция по готовке пельменей</h1>
+				{title}
 				<div className={styles.steps}>
-					<div className={styles['steps-content']}>
-						{/* Для получения активного контента использйте steps и activeIndex */}
-						Контент соответственный шагу. Сейчас активен шаг 3
-					</div>
-					<ul className={styles['steps-list']}>
-						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-							<button className={styles['steps-item-button']}>1</button>
-							{/* При клике на кнопку установка выбранного шага в качестве активного */}
-							Шаг 1
-						</li>
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							<button className={styles['steps-item-button']}>2</button>
-							Шаг 2
-						</li>
-						<li
-							className={
-								styles['steps-item'] +
-								' ' +
-								styles.done +
-								' ' +
-								styles.active
-							}
-						>
-							<button className={styles['steps-item-button']}>3</button>
-							Шаг 3
-						</li>
-						<li className={styles['steps-item']}>
-							<button className={styles['steps-item-button']}>4</button>
-							Шаг 4
-						</li>
-					</ul>
+					{content}
+					<ul className={styles['steps-list']}>{itemPitch}</ul>
 					<div className={styles['buttons-container']}>
-						<button className={styles.button}>Назад</button>
-						<button className={styles.button}>
-							Далее
-							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-							{/* Или заменять всю кнопку в зависимости от условия */}
+						<button
+							onClick={clickDown}
+							className={styles.button}
+							disabled={dis}
+						>
+							Назад
 						</button>
+						<button onClick={clickUp} className={styles.button}>
+							{activeIndex < steps.length ? 'Далее' : 'Начать сначала'}
+						</button>
+						{activeIndex}
 					</div>
 				</div>
 			</div>
